597af6833e29f8752cd5f828144de82f94f77997
Zindrek <bms1991@gmail.com>
2022-04-08 17:53:39 -0400

    removed unused placeholder components

--- src/interfaces/unionTypes/IStringDescription.ts
+++ src/interfaces/unionTypes/IStringDescription.ts
@@ -2,4 +2,4 @@
 /**
  * a union type of all possible values for UI Labels for an IIMPString
  */
-export type IStringDescription = `name` | `Out of Stock`
+export type IStringDescription = `name` | `Out of Stock` | `Written in` | `Author` | `Tester` | `Documenter` | `Copyright Date` | `Description` | `Version` | `Login Required`
