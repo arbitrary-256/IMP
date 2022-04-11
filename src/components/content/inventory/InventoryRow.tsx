3202de3b5e995388191607aa94bdf6056bdc2184
Zindrek <bms1991@gmail.com>
2022-04-11 19:25:00 -0400

    simplified item keys

--- src/components/content/inventory/InventoryRow.tsx
+++ src/components/content/inventory/InventoryRow.tsx
@@ -11,27 +11,28 @@ import { StringChanger } from '../../shared/StringChanger'
  * @returns a React.FC that displays one row of the Inventory tab
  */
 export const InventoryRow: React.FC<IIMPProduct> = (product: IIMPProduct): React.ReactElement => {
+  const upcIdentifier = product.upc.value.toString()
   return (
-    <TableRow className={`InventoryRow`} key={`InventoryRow${product.upc.value}`}>
-      <TableCell key={`UPCCode${product.upc.value}`} width={`8%`}>
+    <TableRow className={`InventoryRow`} key={`InventoryRow${upcIdentifier}`}>
+      <TableCell key={`upc${upcIdentifier}`} width={`8%`}>
         {NumberDisplayer(product.upc)}
       </TableCell>
-      <TableCell key={`UPCCode${product.name.text}`} width={`20%`}>
+      <TableCell key={`name${upcIdentifier}`} width={`20%`}>
         {StringChanger({ string: product.name, newValue: product.name.text, product: product })}
       </TableCell>
-      <TableCell key={`min${product.upc.value}`} width={`12%`}>
+      <TableCell key={`min${upcIdentifier}`} width={`12%`}>
         {NumberChanger({ number: product.min, product: product })}
       </TableCell>
-      <TableCell key={`onHand${product.upc.value}`} width={`12%`}>
+      <TableCell key={`onHand${upcIdentifier}`} width={`12%`}>
         {NumberChanger({ number: product.onHand, product: product })}
       </TableCell>
-      <TableCell key={`max${product.upc.value}`} width={`12%`}>
+      <TableCell key={`max${upcIdentifier}`} width={`12%`}>
         {NumberChanger({ number: product.max, product: product })}
       </TableCell>
-      <TableCell key={`price${product.upc.value}`} width={`12%`}>
+      <TableCell key={`price${upcIdentifier}`} width={`12%`}>
         {NumberChanger({ number: product.price, product: product })}
       </TableCell>
-      <TableCell key={`cost${product.upc.value}`} width={`12%`}>
+      <TableCell key={`cost${upcIdentifier}`} width={`12%`}>
         {NumberChanger({ number: product.cost, product: product })}
       </TableCell>
     </TableRow>
