5b6535b2e2eec8e0462bfa586a6c2ae32d984b10
Zindrek <bms1991@gmail.com>
2022-04-13 10:28:33 -0400

    updated JSDoc comment

--- src/functions/helpers/receiveInventory.ts
+++ src/functions/helpers/receiveInventory.ts
@@ -3,13 +3,13 @@
 import { IIMPProduct } from '../../interfaces/IIMPProduct'
 /**
  * adds inventory to the local application state
- * @param product the product to receive
+ * @param product state.productToReceive
  * @param inventory state.inStock
  * @returns a new IIMPProduct[] with the product's inventory added
  */
 export const receiveInventory: Function = (product: IIMPProduct, inventory: IIMPProduct[]): IIMPProduct[] => {
   let newInventory: IIMPProduct[] = { ...inventory }
-  const productFromInventory: IIMPProduct | undefined = newInventory.find((productToFind) => productToFind.upc === product.upc)
+  const productFromInventory: IIMPProduct | undefined = newInventory.find((productToFind) => productToFind.upc.value === product.upc.value)
   const inventoryIndex: number = newInventory.findIndex((productToFindIndexOf) => productToFindIndexOf.upc === product.upc)
   if (productFromInventory && inventoryIndex !== -1) {
     // if product is in inventory, add to quantity in inventory
