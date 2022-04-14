198cf2a193361d08d42478a8fba2512e686155d6
Zindrek <bms1991@gmail.com>
2022-04-14 17:58:12 -0400

    fixed reducerCleanup

--- src/functions/helpers/reducerCleanup.ts
+++ src/functions/helpers/reducerCleanup.ts
@@ -13,8 +13,7 @@ import { generateProduct } from './generateProduct'
  * @returns the cleaned up application state
  */
 export const reducerCleanup: Function = (newState: IIMPState, oldState?: IIMPState, product?: IIMPProduct): IIMPState => {
-  let productIndex: number = -1
-  
+  let productIndex: number = -2
   /**
    * empties the cart and generates a new productToReceive
    */
@@ -26,25 +25,28 @@ export const reducerCleanup: Function = (newState: IIMPState, oldState?: IIMPSta
     newState.stateFuture = []
     newState.stateHistory = []
   }
-  
   /**
    * locates a product in state
    */
-  const locateAndPlaceProduct: Function = (): void => {
-    // if product parameter was passed, find it's index in the inStock array
-    product ? productIndex = newState.inStock.findIndex((productToFind) => productToFind.upc.value === product.upc.value) : void 0
-    // if productIndex is not -1, set the productToLocate to the product
-    productIndex && productIndex !== -1 && product ? newState.inStock[productIndex] = product : void 0
-    // if the productIndex is -1 and the upc matches the productToReceive, set the productToReceive to the product
-    productIndex === -1 && product && product.upc.value === newState.productToReceive.upc.value ? newState.productToReceive = product : void 0
+  const locateAndPlaceProduct: Function = (product: IIMPProduct): void => {
+    const nameMatchesReceiving: boolean = newState.productToReceive.name === product.name
+    const upcMatchesReceiving: boolean = newState.productToReceive.upc.value === product.upc.value
+    const matchesReceiving: boolean = nameMatchesReceiving || upcMatchesReceiving
+    // if product parameter was passed, find it's index in the inStock array by upc or name
+    productIndex = newState.inStock.findIndex((productToFind) => productToFind.upc.value === product.upc.value || productToFind.name === product.name)
+    // if product was found and matched, set newState.inStock[productToLocate] to the product
+    productIndex > -1 ? (newState.inStock[productIndex] = product) : void 0
+    // if product was not found and matches the product in receiving, set the productToReceive to the product
+    productIndex === -1 && matchesReceiving ? (newState.productToReceive = product) : void 0
   }
-
+  // if the upc in receiving matches any upc in inventory, set the productToReceive to the product in inventory
+  newState.inStock.find((productToFind) => newState.productToReceive.upc.value === productToFind.upc.value ? newState.productToReceive = productToFind : void 0)
   // if user is logged out, cleanup user activity
   newState.loggedInUser === `Logged Out` ? logoutCleanup() : void 0
   // if there is an old state, add it to the stateHistory
   oldState ? newState.stateHistory.push(oldState) : void 0
   // if there is a product, place it in the correct place in newState
-  product ? locateAndPlaceProduct() : void 0
+  product ? locateAndPlaceProduct(product) : void 0
   // rebuild the contents of the cart
   newState.cart = newState.inStock.filter((productInInventory) => productInInventory.inCart.value > 0)
   return newState
