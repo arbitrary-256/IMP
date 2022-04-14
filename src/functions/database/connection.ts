df24fe51a6e12e0d5c107999228a9cecf336a65e
Zindrek <bms1991@gmail.com>
2022-04-14 19:19:41 -0400

    added .env support

--- src/functions/database/connection.ts
+++ src/functions/database/connection.ts
@@ -1,19 +1,22 @@
 /** @format */
+// libraries
 import mariadb from 'mariadb'
+import dotenv from 'dotenv'
 /**
  * creates a connection to the database with appropriate credentials
  * @returns a connection to the database
  */
 const mariaCredentials: Function = (): mariadb.ConnectionConfig => {
+  dotenv.config()
   return {
     // the host address of the mariadb or mysql server - defaults to port 3306, set as ${IP}:${PORT}
-    host: `your.mariadb.host`,
+    host: process.env.IMPHOST,
     // the user name to authenticate as
-    user: `impdaemoncritter`,
+    user: process.env.IMPUSERNAME,
     // the password to use to authenticate
-    password: `IMPimpI1Y1P!^^P`,
+    password: process.env.IMPPASSWORD,
     // the database to connect to
-    database: `impbase`
+    database: process.env.IMPDATABASE
   }
 }
 export const connection: mariadb.Connection = await mariadb.createConnection(mariaCredentials())
