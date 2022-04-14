f2896abecb87be293c17cbb400c619528ec2b1fd
Zindrek <bms1991@gmail.com>
2022-04-14 15:28:06 -0400

    implemented edit and delete functionality

---
+++ src/functions/helpers/deleteProduct.ts
@@ -0,0 +1,15 @@
+/** @format */
+// interfaces
+import { IIMPProduct } from '../../interfaces/IIMPProduct'
+// non-TypeScript data
+/**
+ * toggles the editability of a product
+ * @param product the product to delete
+ * @returns the new anventory Array
+ */
+export const deleteProduct: Function = (product: IIMPProduct, inventory: IIMPProduct[]): IIMPProduct[] => {
+  // find index of product in inventory
+  const index: number = inventory.findIndex((item: IIMPProduct) => item.upc.value === product.upc.value)
+  // remove product in inventory
+  return [...inventory.slice(0, index), ...inventory.slice(index + 1)]
+}
---
+++ src/functions/helpers/editProduct.ts
@@ -0,0 +1,17 @@
+/** @format */
+// interfaces
+import { IIMPProduct } from '../../interfaces/IIMPProduct'
+// non-TypeScript data
+/**
+ * toggles the editability of a product
+ * @param product the product to make editable or not
+ * @returns the new inventory Array
+ */
+export const editProduct: Function = (product: IIMPProduct, inventory: IIMPProduct[]): IIMPProduct[] => {
+  const toggledMode: boolean = !product.editingMode
+  const toggledProduct: IIMPProduct = { ...product, editingMode: toggledMode }
+  // find index of product in inventory
+  const index: number = inventory.findIndex((item: IIMPProduct) => item.upc.value === product.upc.value)
+  // replace product in inventory
+  return [...inventory.slice(0, index), toggledProduct, ...inventory.slice(index + 1)]
+}
