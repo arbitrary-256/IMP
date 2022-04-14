f6c4d3485cdb2b57239b62d92b5a1a63330a94ec
Zindrek <bms1991@gmail.com>
2022-04-14 19:40:25 -0400

    added root username and password

--- src/scripts/utility/testDotenv.ts
+++ src/scripts/utility/testDotenv.ts
@@ -4,6 +4,9 @@ dotenv.config()
 
 export const testDotenv: Function = (): void => {
   !process.env.IMPHOST ? console.error(`no IMPHOST found in .env`) : console.log(`IMPHOST: ${process.env.IMPHOST}`)
+  !process.env.IMPPORT ? console.error(`no IMPPORT found in .env`) : console.log(`IMPHOST: ${process.env.IMPPORT}`)
+  !process.env.IMPROOTUSERNAME ? console.error(`no IMPROOTUSERNAME found in .env`) : console.log(`IMPROOTUSERNAME: ${process.env.IMPROOTUSERNAME}`)
+  !process.env.IMPROOTPASSWORD ? console.error(`no IMPROOTPASSWORD found in .env`) : console.log(`IMPROOTPASSWORD: ${process.env.IMPROOTPASSWORD}`)
   !process.env.IMPUSERNAME ? console.error(`no IMPUSERNAME found in .env`) : console.log(`IMPUSERNAME: ${process.env.IMPUSERNAME}`)
   !process.env.IMPPASSWORD ? console.error(`no IMPPASSWORD found in .env`) : console.log(`IMPPASSWORD: ${process.env.IMPPASSWORD}`)
   !process.env.IMPDATABASE ? console.error(`no DATABASE found in .env`) : console.log(`IMPDATABASE: ${process.env.IMPDATABASE}`)
