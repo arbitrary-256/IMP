b807f59806e985c37a9a5c4cb57c4813c2a29ee8
Zindrek <bms1991@gmail.com>
2022-04-17 15:33:21 -0400

    Update gif.d.ts

--- src/typings/gif.d.ts
+++ src/typings/gif.d.ts
@@ -2,12 +2,3 @@
 declare module '*.gif' {
   export const value: string
 }
-declare module '*.jpg' {
-  export const value: string
-}
-declare module '*.png' {
-  export const value: string
-}
-declare module '*.svg' {
-  export const value: string
-}
