8958cc0fad1ef1f4fa4900ef7bf815fa7b81e107
Zindrek <bms1991@gmail.com>
2022-04-18 14:37:49 -0400

    fleshed out .env file

--- src/scripts/utility/testDotenv.ts
+++ src/scripts/utility/testDotenv.ts
@@ -3,8 +3,10 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const testDotenv: Function = (): void => {
-  !process.env.IMPHOST ? console.error(`no IMPHOST found in .env`) : console.log(`IMPHOST: ${process.env.IMPHOST}`)
-  !process.env.IMPPORT ? console.error(`no IMPPORT found in .env`) : console.log(`IMPHOST: ${process.env.IMPPORT}`)
+  !process.env.IMPSTACKNAME ? console.error(`no IMPSTACKNAME found in .env`) : console.log(`IMPSTACKNAME: ${process.env.IMPSTACKNAME}`)
+  !process.env.IMPMARIADBHOST ? console.error(`no IMPMARIADBHOST found in .env`) : console.log(`IMPMARIADBHOST: ${process.env.IMPMARIADBHOST}`)
+  !process.env.IMPCONTAINERPORT ? console.error(`no IMPCONTAINERPORT found in .env`) : console.log(`IMPCONTAINERPORT: ${process.env.IMPCONTAINERPORT}`)
+  !process.env.IMPEXTERNALPORT ? console.error(`no IMPEXTERNALPORT found in .env`) : console.log(`IMPEXTERNALPORT: ${process.env.IMPEXTERNALPORT}`)
   !process.env.IMPROOTUSERNAME ? console.error(`no IMPROOTUSERNAME found in .env`) : console.log(`IMPROOTUSERNAME: ${process.env.IMPROOTUSERNAME}`)
   !process.env.IMPROOTPASSWORD ? console.error(`no IMPROOTPASSWORD found in .env`) : console.log(`IMPROOTPASSWORD: ${process.env.IMPROOTPASSWORD}`)
   !process.env.IMPUSERNAME ? console.error(`no IMPUSERNAME found in .env`) : console.log(`IMPUSERNAME: ${process.env.IMPUSERNAME}`)
