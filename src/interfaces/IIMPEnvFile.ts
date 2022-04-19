5a685088493b756f49caecdfdfb8852a16f49bdd
Zindrek <bms1991@gmail.com>
2022-04-19 15:41:56 -0400

    Create IIMPEnvFile.ts

---
+++ src/interfaces/IIMPEnvFile.ts
@@ -0,0 +1,13 @@
+/** @format */
+/**
+ * an interface for the data stored in a .env file
+ */
+export interface IIMPEnvFile {
+  hostnameString: string
+  portNumber: number
+  rootUserNameString: string
+  rootPasswordString: string
+  userNameString: string
+  passwordString: string
+  databaseNameString: string
+}
