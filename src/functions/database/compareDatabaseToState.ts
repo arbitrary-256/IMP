23f4d3f39183393792c52f9d36d824c317387304
Zindrek <bms1991@gmail.com>
2022-04-20 02:59:51 -0400

    removed pointless commented lines

--- src/functions/database/compareDatabaseToState.ts
+++ src/functions/database/compareDatabaseToState.ts
@@ -6,12 +6,5 @@ export const compareDatabaseToState = async (current: IIMPProduct[], last: IIMPP
   // request the differences between the database and the local changes from connection
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const remoteChanges = await userConnection.query(`SELECT * FROM products WHERE id IN (${localChanges.map((product) => product.upc).join(',')})`)
-  // const submitDifference: Function = () =>
-  //   difference.map((product) => {
-  //     const thisQuery = `INSERT INTO products (id, name, price, quantity, description, image) VALUES ('${product.upc}', '${product.name}', '${product.price}', '${product.cost}', '${product.inStock}', '${product.min}', '${product.max}', '${product.image}')`
-  //     connection.query(thisQuery)
-  //     return void 0
-  //   })
-  // await submitDifference()
   // connection.end()
 }
