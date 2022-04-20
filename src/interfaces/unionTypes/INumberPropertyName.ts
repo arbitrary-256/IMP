4e585692d24d373ea82a8c8844df4df03f2e60d8
Zindrek <bms1991@gmail.com>
2022-04-20 02:57:52 -0400

    added timestamp-relates string literals to unions

--- src/interfaces/unionTypes/INumberPropertyName.ts
+++ src/interfaces/unionTypes/INumberPropertyName.ts
@@ -2,4 +2,23 @@
 /**
  * a union type of all possible property names for an IIMPNumber in an IIMPProduct
  */
-export type INumberPropertyName = `Available` | `Cart Total` | `Item Total` | `cost` | `price` | `inStock` | `upc` | `inCart` | `toReceive` | `min` | `max`
+export type INumberPropertyName =
+  | `Available`
+  | `Cart Total`
+  | `Item Total`
+  | `cost`
+  | `price`
+  | `inStock`
+  | `UPC`
+  | `inCart`
+  | `toReceive`
+  | `min`
+  | `max`
+  | `dateAdded`
+  | `timeAdded`
+  | `dateModified`
+  | `timeModified`
+  | `dateFirstSold`
+  | `timeFirstSold`
+  | `dateLastSold`
+  | `timeLastSold`
--- src/interfaces/unionTypes/IStringDescription.ts
+++ src/interfaces/unionTypes/IStringDescription.ts
@@ -2,4 +2,20 @@
 /**
  * a union type of all possible values for UI Labels for an IIMPString
  */
-export type IStringDescription = `name` | `Out of Stock` | `Written in` | `Coding` | `Testing` | `Documentation` | `Copyright Date` | `Description` | `Version` | `Login Required`
+export type IStringDescription =
+  | `Product Name`
+  | `Out of Stock`
+  | `Written in`
+  | `Coding`
+  | `Testing`
+  | `Documentation`
+  | `Copyright Date`
+  | `Description`
+  | `Version`
+  | `Login Required`
+  | `Date Added`
+  | `Time Added`
+  | `Date Modified`
+  | `Time Modified`
+  | `Date Last Sold`
+  | `Time Last Sold`
