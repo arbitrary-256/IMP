88bad91ddd9fc7d55721bbb2900f0570057213ec
Zindrek <bms1991@gmail.com>
2022-04-20 03:02:33 -0400

    changed to use IIMPDatabaseRow

--- src/functions/database/updateDatabaseEntry.ts
+++ src/functions/database/updateDatabaseEntry.ts
@@ -1,22 +1,21 @@
 /** @format */
-import { IIMPProduct } from '../../interfaces/IIMPProduct'
+import { IIMPDatabaseRow } from '../../interfaces/IIMPDatabaseRow'
 import { userConnection } from './userConnection'
 /**
  * updates a database entry for a given product
  * @param product the product to update in the database
  * @returns a promise that resolves when the product is updated in the database
  */
-export const updateDatabaseEntry = async (product: IIMPProduct): Promise<void> => {
+export const updateDatabaseEntry = async (product: IIMPDatabaseRow): Promise<void> => {
   await userConnection.query(`UPDATE products SET
-        upc = '${product.upc.value}',
-        name = '${product.name.text}',
-        cost = '${product.cost.value}',
-        salePrice = '${product.price.value}',
-        min = '${product.min.value}',
-        max = '${product.max.value}',
-        inStock = '${product.inStock.value}',
-        inCart = '${product.inCart.value}',
-        image = '${product.image}'
-    WHERE id = ${product.upc.value}`)
+        upc = '${product.upcValue}',
+        name = '${product.nameText}',
+        cost = '${product.costValue}',
+        salePrice = '${product.priceValue}',
+        min = '${product.minValue}',
+        max = '${product.maxValue}',
+        inStock = '${product.inStockValue}',
+        image = '${product.imageData}'
+    WHERE id = ${product.upcValue}`)
   userConnection.end()
 }
