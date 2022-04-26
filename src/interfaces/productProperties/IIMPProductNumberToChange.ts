9321cae14a3c8d0a849813d043ebb5d6adb65d4a
Zindrek <bms1991@gmail.com>
2022-04-26 13:25:24 -0400

    added JSDoc comments

--- src/interfaces/productProperties/IIMPProductNumberToChange.ts
+++ src/interfaces/productProperties/IIMPProductNumberToChange.ts
@@ -2,6 +2,11 @@
 // interfaces
 import { IIMPNumber } from '../IIMPNumber'
 import { IIMPProduct } from '../IIMPProduct'
+/**
+ * changes the value of a number in a product
+ * @param product an IIMPProduct with a number that needs to be changed
+ * @param number an IIMPNumber that needs to change
+ */
 export interface IIMPProductNumberToChange {
   number: IIMPNumber
   product: IIMPProduct
--- src/interfaces/productProperties/IIMPProductStringToChange.ts
+++ src/interfaces/productProperties/IIMPProductStringToChange.ts
@@ -2,8 +2,14 @@
 // interfaces
 import { IIMPProduct } from '../IIMPProduct'
 import { IIMPString } from '../IIMPString'
+/**
+ * interface for a string to change in state
+ * @param product an IIMPProduct with a string that needs to be changed
+ * @param string the IIMPString property to change in product
+ * @param newValue the new string.text value
+ */
 export interface IIMPProductStringToChange {
+  product: IIMPProduct
   string: IIMPString
   newValue: string
-  product: IIMPProduct
 }
