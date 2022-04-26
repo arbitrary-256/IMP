ca0c299b059e13987a8e8a0780db4ade1870b121
Zindrek <bms1991@gmail.com>
2022-04-26 13:21:49 -0400

    simplified implementation

--- src/functions/helpers/setString.ts
+++ src/functions/helpers/setString.ts
@@ -2,18 +2,14 @@
 // interfaces
 import { IIMPProduct } from '../../interfaces/IIMPProduct'
 import { IIMPProductStringToChange } from '../../interfaces/productProperties/IIMPProductStringToChange'
-import { IIMPString } from '../../interfaces/IIMPString'
 /**
  * changes the string in a product to a value typed in by the user
  * @param toChange { string, an IIMPString, value, a string primitive, and product, an IIMPProduct }
  * @returns the updated product
  */
 export const setString: Function = (toChange: IIMPProductStringToChange): IIMPProduct => {
-  let newProduct: IIMPProduct = { ...toChange.product }
-  let newString: IIMPString = { ...toChange.string }
-  newString.text = toChange.newValue
   return {
-    ...newProduct,
-    [newString.id]: newString
+    ...toChange.product,
+    [toChange.string.id]: { ...toChange.string, text: toChange.newValue }
   }
 }
