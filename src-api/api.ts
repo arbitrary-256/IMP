8fedf638ef17fdc44334e63c71de5386b3bedecc
Zindrek <bms1991@gmail.com>
2022-05-01 14:32:03 -0400

    trying a different api theory

--- src-api/api.ts
+++ src-api/api.ts
@@ -2,90 +2,110 @@
 // libraries
 import { config } from 'dotenv'
 import * as express from 'express'
-import * as sql from 'mysql2'
-
-// initial server setup
-console.log(`starting IMP api.ts logs:`)
-config() // load .env file
-
-// make timestamps function
-const timestamp: Function = () => new Date().toLocaleString()
-
-// create sql.ConnectionOptions from environment variables
-const connectAs: Function = (user: `root` | `regular`): sql.ConnectionOptions => {
-  // timestamp log
-  console.log(`conifguring database connection for ${user} at ${timestamp()}`)
-  return {
-    host: `imp-${process.env.REACT_APP_MARIADBHOST}-1`,
-    user: user === `root` ? process.env.REACT_APP_ROOTUSER.toString() : process.env.REACT_APP_USER.toString(),
-    password: user === `root` ? process.env.REACT_APP_ROOTPASSWORD.toString() : process.env.REACT_APP_PASSWORD.toString(),
-    database: process.env.REACT_APP_DATABASENAME,
-    port: 3306,
-    multipleStatements: true,
-    connectionLimit: 10,
-    waitForConnections: true,
-    queueLimit: 0,
-    connectTimeout: 10000,
-    dateStrings: true,
-    supportBigNumbers: true,
-    bigNumberStrings: true
+import { Connection, Query } from 'mysql2'
+import { mariaConfigureAs } from './src/mariaConfigureAs'
+import { mariaConnectAs } from './src/mariaConnectAs'
+// use .env values in this file
+config()
+/**
+ * connects to the MariaDB database as the root user in .env
+ * @returns a root connection ready to use
+ */
+const connectAsRoot: Function = async (): Promise<Connection | undefined> => {
+  try {
+    console.log(`awaiting connection promise`)
+    const connection: Connection = await mariaConnectAs(mariaConfigureAs(`root`))
+    console.log(connection.config)
+    console.log(`connected to MariaDB as root user: ${connection.config.user}`)
+    return connection
+  } catch (error) {
+    console.log(`error: ${error}`)
+    return undefined
   }
 }
+const showDatabases: Function = async (): Promise<void> => {
+  console.log(`connecting to MariaDB as root`)
+  rootConnection = await connectAsRoot()
+  console.log(`connected to MariaDB as root user: ${rootConnection.config.user}`)
+  console.log(`getting database names`)
+  const showDatabasesQuery: Query = rootConnection.query(`SHOW DATABASES`).on('error', (error: Error) => {
+    console.log(`error: ${error}`)
+  }).on('result', (row: any) => {
+    console.log(`database: ${row.Database}`)
+  })
+  // add basic listeners to the connection
+  rootConnection.on('end', (): void => {
+    console.log(`end of query`)
+  }).on('error', (error: Error): void => {
+    console.log(`error: ${error}`)
+  }).on('fields', (fields: any): void => {
+    console.log(`fields: ${fields}`)
+  }).on('result', (row: any): void => {
+    console.log(`row: ${row}`)
+  }).on('resultset', (resultSet: any): void => {
+    console.log(`resultSet: ${resultSet}`)
+  }).on('row', (row: any): void => {
+    console.log(`row: ${row}`)
+  })
+  // use rootConnection to execute the sql query
+  console.log(`executing query`)
+  rootConnection.execute(showDatabasesQuery)
+}
 
-// create Express server Application
-const apiServer: express.Application = express()
+console.log(`starting IMP api.ts logs:`)
 
-// create api response headers
+// let regularConnection: Connection
+let rootConnection: Connection
+showDatabases()
+
+
+rootConnection = connectAsRoot()
+console.log(rootConnection)
+
+// const disconnectUser: Function = (connectionToEnd: Connection): void => {
+//   try {
+//     connectionToEnd.end()
+//     console.log(`disconnected from MariaDB as user: ${connectionToEnd.threadId}`)
+//   } catch (error) {
+//     console.log(`error: ${error}`)
+//   }
+// }
+
+// disconnectUser(rootConnection)
+// disconnectUser(regularConnection)
+
+// create express application
+const apiServer: express.Application = express.application
+// enable json responses
+apiServer.use(express.json)
+// create api response header
 apiServer.head(`/`, (req: express.Request, res: express.Response) => {
   res.set(`Content-Type`, `application/json`)
-  res.set(`Access-Control-Allow-Origin`, `*`)
-  res.set(`Access-Control-Allow-Methods`, `GET, POST, PUT, DELETE, OPTIONS`)
-  res.set(`Access-Control-Allow-Headers`, `Origin, X-Requested-With, Content-Type, Accept, Authorization`)
-  res.set(`Access-Control-Allow-Credentials`, `true`)
-  res.set(`Access-Control-Max-Age`, `86400`)
+  res.set(`Access-Control-Allow-Methods`, `GET, POST, PUT, DELETE`)
   res.send(`OK`)
 })
-
-// create an endpoint for the api to list it's routes at route `/routes`
-apiServer.get(`/routes`, (req: express.Request, res: express.Response): void => {
-  // timestamp in console.log
-  console.log(`requesting route list at ${timestamp()}`)
-  res.json({
-    routes: [`/routes`, `/checkConnection`]
-  })
-  console.log(`responded to route list at ${timestamp()}`)
+// create GET endpoint at `/hello`
+apiServer.get(`/hello`, (req: express.Request, res: express.Response): void => {
+    res.status(200).send(`/hey there!`)
+  console.log(`printed hello world`)
 })
-
-// create an endpoint for the api to validate an sql connection
-apiServer.get(`/checkConnection`, (req: express.Request, res: express.Response): void => {
-  // timestamp in console.log
-  console.log(`validating connection at ${timestamp()}`)
-  // create a connection to the database as root user
-  const rootConnection: sql.Connection = sql.createConnection(connectAs(`root`))
-
-  rootConnection.connect((err: Error) => {
-    // if there is an error, log it and return a 500 status code
-    if (err) {
-      console.log(`error connecting to database at ${timestamp()}`)
-      console.log(err)
-      res.status(500).send(`error connecting to database`)
-    } else {
-      // if there is no error, log the connection and return a 200 status code
-      console.log(`connected to database at ${timestamp()}`)
-      res.status(200).send(`connected to database`)
-    }
-  })
-
-  // close root connection
-  rootConnection.end()
-
-  let rootResult: boolean | undefined = undefined
-  // end all connections
-  rootConnection.end()
-  // console.log results
-  rootResult && console.log(`connected as root: ${rootResult}`)
+// create GET endpoint at /showRoutes
+apiServer.get(`/showRoutes`, (req: express.Request, res: express.Response): void => {
+    res.status(200)
+        .json({ routes: [`/hello`, `/routes`] })
+        .send()
+    console.log(`returned showRoutes JSON`)
 })
+// apiServer.get(`/beRoot`, async (req: express.Request, res: express.Response): Promise<void> => {
+//     console.log(`${new Date().toLocaleString()}: validating connection as root`)
+//     await mariaConnectAs(mariaConfigureAs(`root`))
+// })
+// apiServer.get(`/beUser`, async (req: express.Request, res: express.Response): Promise<void> => {
+//     console.log(`${new Date().toLocaleString()}: validating connection as user`)
+//     await mariaConnectAs(mariaConfigureAs(`user`))
+// })
 
-apiServer.listen(process.env.REACT_APP_APIPORT, () => {
-  console.log(`IMP API server listening on port ${process.env.REACT_APP_APIPORT}`)
+// start server
+apiServer.listen(parseInt(`${process.env.REACT_APP_APIPORT}`), () => {
+  console.log(`${new Date().toLocaleString()}: IMP API server listening on port ${process.env.REACT_APP_APIPORT}`)
 })
