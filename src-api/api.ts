ec5ac2907dc78a99fd79204cbc99657e885cddd6
Zindrek <bms1991@gmail.com>
2022-05-02 01:41:03 -0400

    refactor starts

--- src-api/api.ts
+++ src-api/api.ts
@@ -2,65 +2,21 @@
 // libraries
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as http from 'http'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as bodyParser from 'body-parser'
 import { config } from 'dotenv'
-import * as express from 'express'
+import { application, Application, Router } from 'express'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as mysql from 'mysql'
 // express pieces
-import { apiRouter } from './src/routes/apiRouter'
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { serverHealthCheck } from './src/functions/serverHealthCheck'
+// import { apiRouter } from './src/routes/apiRouter'
 // use .env values in this file
 config()
-
+const apiPort = parseInt(process.env.REACT_APP_APIPORT!)
 // create express application
-const apiServer: express.Application = express.application
+const apiServer: Application = application
 
-// apiServer should use apiRouter to handle requests
-apiServer.use('/api', apiRouter)
-
-// log requests to the router
-apiRouter.use((request: express.Request, response: express.Response, next: express.NextFunction) => {
-  console.log(`request: ${request.method} ${request.url}`)
-  next()
+// start api server
+apiServer.listen(apiPort, () => {
+  console.log(`${new Date().toLocaleString()}: API listening on port ${apiPort}`)
 })
-
-// parse request body in the router
-apiRouter.use(bodyParser.json())
-apiRouter.use(bodyParser.urlencoded({ extended: true }))
-
-// allow connections from any origin
-apiRouter.use((request: express.Request, response: express.Response, next: express.NextFunction) => {
-  response.header('Access-Control-Allow-Origin', '*')
-  response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
-
-// create a GET endpoint at / to show available routes
-apiRouter.get(`/api`, async (request: express.Request, response: express.Response) => {
-  console.log(`someone asked for / (to show routes, probably)`)
-  response
-    .status(200)
-    .json({
-      message: `available routes:`,
-      routes: [`/api`, `/api/hello`]
-    })
-    .send()
-})
-
-// create GET endpoint at `/hello`
-apiRouter.get(`/api/hello`, (req: express.Request, res: express.Response): void => {
-  console.log(`someone asked for /hello`)
-  res.status(200).json({ message: `/hey there, stranger!` }).send()
-})
-
-// enable json responses
-apiServer.use(express.json)
-
-// start server
-apiServer.listen(parseInt(`${process.env.REACT_APP_APIPORT}`), () => {
-  console.log(`${new Date().toLocaleString()}: API listening on port ${process.env.REACT_APP_APIPORT}`)
-})
-
-export { apiServer }
--- src-api/src/routes/apiRouter.ts
+++
@@ -1,9 +0,0 @@
-/** @format */
-// libraries
-import { Router } from 'express'
-// controllers
-import { serverHealthCheck } from '../functions/serverHealthCheck'
-
-export const apiRouter = Router()
-
-apiRouter.get(`/ping`, serverHealthCheck())
