f141de4eee3c72ee5627b26d4a4cf4510dca748b
Zindrek <bms1991@gmail.com>
2022-05-02 11:48:06 -0400

    another take

--- src-api/impAPI.ts
+++ src-api/impAPI.ts
@@ -1,36 +1,14 @@
 /** @format */
 // libraries
-import { application, Application, json, Request, Response, /*Router,*/ urlencoded } from 'express'
-// utility
+import { application, Application, Request, Response } from 'express'
+// our code
 import { environment } from './src/utility'
-// logs
 import { i } from './src/logs'
-// connection
-import {} from './src/connection'
-// express
-import {} from './src/express'
 
 const impAPI: Application = application
-// do stuff with the api server, like add routes
-// routers
-impAPI.use(urlencoded)
-impAPI.use(json)
-// const getRouter: Router = Router()
-// impAPI.use(getRouter)
-// getRouter.get(`/get`, (request: Request, response: Response) => {
-//   response
-//     .status(200)
-//     .json({
-//       message: `you GETted at ${request.url}`
-//     })
-//     .send()
-// })
-// getRouter.get(`/hello`, (request: Request, response: Response) => {
-//   i(`routing`, `/hello`)
-//   response.status(200).json({ message: `Hello, world!` }).send()
-// })
-impAPI.get(`*`, (request: Request, response: Response) => {
+impAPI.listen(environment.apiServer.port, () => i(`api`, `Listening on ${environment.apiServer.host}:${environment.apiServer.port}`))
+
+impAPI.routes.get(`/`, (request: Request, response: Response) => {
   i(`routing`, `${request.url}`, { ...request })
-  response.status(200).json({ message: `Hello, world!` }).send()
+  response.send(`ollie, ollie, oxen-free`)
 })
-impAPI.listen(environment.apiServer.port, () => i(`operation`, `Listening on ${environment.apiServer.host}:${environment.apiServer.port}`))
