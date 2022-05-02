c207bb82078f2dddcd7220fae87e9d8c20b39f21
Zindrek <bms1991@gmail.com>
2022-05-02 07:07:17 -0400

    general cleanup

--- src/interfaces/unionTypes/IPrimitiveWrapper.ts
+++
@@ -1,9 +0,0 @@
-/** @format */
-// interfaces
-import { IIMPImage } from '../IIMPImage'
-import { IIMPNumber } from '../IIMPNumber'
-import { IIMPString } from '../IIMPString'
-/**
- * a union type of all possible values for an IIMP primitive wrapper interface
- */
-export type IPrimitiveWrapper = IIMPImage | IIMPNumber | IIMPString
