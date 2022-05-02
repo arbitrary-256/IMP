348c5469edc95a49aad83837d3f1180f9705bbd3
Zindrek <bms1991@gmail.com>
2022-05-02 00:35:13 -0400

    Delete serverHealthCheck.ts

--- src-api/src/functions/serverHealthCheck.ts
+++
@@ -1,7 +0,0 @@
-/** @format */
-// libraries
-import { Request, Response } from 'express'
-// controllers
-export const serverHealthCheck: Function = (request: Request, response: Response) => {
-  response.status(200).json({ message: `/ping` }).send()
-}
